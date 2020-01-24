b49b0e2a5df9ea5b240e94c7152be3286b0f5673
Arsen <arsen@Arsens-MacBook-Pro.local>
2020-01-24 10:45:45 +0100

    all posts page

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="container">
-    <ul>
-      <li v-for="post in allPosts" :key="post.id">
-        {{ post.title }}
+    <div class="mt-3">
+      <h1>All Posts</h1>
+    </div>
+    <ul class="list-group mt-5">
+      <li
+        class="list-group-item d-flex flex-column"
+        v-for="post in allPosts"
+        :key="post.id"
+      >
+        <div class="d-flex flex-row align-items-center mt-2">
+          <img :src="post.image" alt="Post image" />
+          <h3 class="m-0 p-0">{{ post.title }}</h3>
+        </div>
+        <div class="mt-4">
+          {{ post.body }}
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { mapGetters }  from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'posts',
+  name: "posts",
   computed: {
     ...mapGetters({
-      allPosts: 'allPosts'
+      allPosts: "allPosts"
     })
   }
-}
+};
 </script>
+
+<style scoped>
+img {
+  max-width: 100px;
+}
+</style>
--- src/store/modules/PostsStore.js
+++ src/store/modules/PostsStore.js
@@ -11,13 +11,13 @@ export const PostsStore = {
                 id: 2,
                 title: 'Post no 2',
                 body: 'This is the content of the second post.',
-                image: 'http://www.onlinetefltraining.com/wp-content/uploads/2013/08/2173808.jpg'
+                image: 'https://www.kurir.rs/data/images/2019/11/27/17/2067251_profimedia0424509037_ls.jpg'
             },
             {
                 id: 3,
                 title: 'Post no 3',
                 body: 'This is the content of the third post.',
-                image: 'http://www.onlinetefltraining.com/wp-content/uploads/2013/08/2173808.jpg'
+                image: 'https://static.makeuseof.com/wp-content/uploads/2018/02/post-panorama-instagram-670x335.jpg'
             }
         ]
     },
